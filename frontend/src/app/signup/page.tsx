3e19522892727c696c1c0c12b25c9f3ab9ab49ea
Axole Maranjana <axolemaranjana4@gmail.com>
2024-04-07 05:34:21 +0200

    Fix sign in and sign up buttons disabled state

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -99,6 +99,7 @@ export default () => {
                     <button
                         className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-primary to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
                         type="submit"
+                        disabled={loading}
                     >
                         <div className="flex items-center justify-center gap-4 text-center">
                             {loading ? "Creating that account..." : "Sign up"}
