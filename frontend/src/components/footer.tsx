eeb941e4d0c50643e223775edada64f57629d03f
Axole Maranjana <axolemaranjana4@gmail.com>
2024-04-15 20:04:29 +0200

    Change version

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,13 +1,33 @@
 export default function Footer() {
-    return (
-        <footer className="flex flex-col bg-background py-3 text-center text-muted/70">
-            <p className="text-sm">
-                &copy; 2024 Doc Chat. All rights reserved. v1.0.0
-            </p>
-            <a target="_blank" href="https://www.freepik.com/free-photo/3d-rendering-zoom-call-avatar_29803186.htm#fromView=search&page=1&position=49&uuid=986f6156-9a4f-483a-bca0-47f2627964e4" rel="noreferrer">Image by freepik</a>
-            <span>
-                <a target="_blank" href="https://icons8.com/icon/SPJrDfRcvJLu/documents" rel="noreferrer">Documents</a> icon by <a target="_blank" href="https://icons8.com" rel="noreferrer">Icons8</a>
-            </span>
-        </footer>
-    )
+	return (
+		<footer className="flex flex-col bg-background py-3 text-center text-muted/70">
+			<p className="text-sm">
+				&copy; 2024 Doc Chat. All rights reserved. v1.1.0
+			</p>
+			<a
+				target="_blank"
+				href="https://www.freepik.com/free-photo/3d-rendering-zoom-call-avatar_29803186.htm#fromView=search&page=1&position=49&uuid=986f6156-9a4f-483a-bca0-47f2627964e4"
+				rel="noreferrer"
+			>
+				Image by freepik
+			</a>
+			<span>
+				<a
+					target="_blank"
+					href="https://icons8.com/icon/SPJrDfRcvJLu/documents"
+					rel="noreferrer"
+				>
+					Documents
+				</a>{" "}
+				icon by{" "}
+				<a
+					target="_blank"
+					href="https://icons8.com"
+					rel="noreferrer"
+				>
+					Icons8
+				</a>
+			</span>
+		</footer>
+	);
 }
