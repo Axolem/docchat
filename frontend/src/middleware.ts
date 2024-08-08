74282d07dd39b32ab8cf2846159e9db2979fb7b5
Axole Maranjana <axolemaranjan4@gmail.com>
2024-08-08 03:25:16 +0200

    Add a counter

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
+
 import type { NextRequest } from "next/server";
+let count = 0;
 
 export function middleware(request: NextRequest) {
+	count++;
+	console.log("A request: #", count);
 	const authToken = request.cookies.get("token");
 	const url = request.nextUrl.clone();
 
